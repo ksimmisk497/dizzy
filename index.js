a68de7f67e4cce4a0cc0161d649c5d91039a5858
crownwm <weswoodland1@gmail.com>
2026-09-16 16:05:41 -0500

    back to duckduckgo

--- index.js
+++ index.js
@@ -47,9 +47,8 @@ async function initUV() {
         return;
       }
     }
-    var template = (engine && engine.value) || "https://www.startpage.com/sp/search?query=%s";
-    if (/search\.brave\.com|brave\.com/i.test(template)) {
-      template = "https://www.startpage.com/sp/search?query=%s";
+    var template = (engine && engine.value) || "https://www.duckduckgo.com/sp/search?query=%s";(/search\.brave\.com|brave\.com/i.test(template)) {
+      template = "https://www.duckduckgo.com/sp/search?query=%s";
     }
     var url = typeof search === "function" ? search(raw, template) : raw;
     location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
